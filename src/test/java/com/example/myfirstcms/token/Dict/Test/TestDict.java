0741ae694e0cf66fc6565e31d495007d0ec17e3a
fanghaoran <horaoen@gmail.com>
2022-11-04 14:06:10 +0800

    resolve package error in test

--- src/test/java/com/example/myfirstcms/token/Dict/Test/TestDict.java
+++ src/test/java/com/example/myfirstcms/token/Dict/Test/TestDict.java
@@ -1,5 +1,6 @@
 package com.example.myfirstcms.token.Dict.Test;
 
+import com.example.myfirstcms.infrastructure.dict.mapper.DictMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
